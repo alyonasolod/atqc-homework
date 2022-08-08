55133b7ea526b17a1a9c7464db16821ef191f7fe
alyonasolod <106539080+alyonasolod@users.noreply.github.com>
2022-08-08 06:52:56 +0300

    Update Android.java

--- src/week2/task4/Android.java
+++ src/week2/task4/Android.java
@@ -27,6 +27,6 @@ public class Android extends Phone {
     }
     @Override
     public String toString(){
-         return "ANDROID "+this.name+" OS "+this.osVersion+" DISPLAY "+this.displaySize+"\""+this.resolution;
+         return "ANDROID "+this.name+" OS "+this.osVersion+" DISPLAY "+this.displaySize+"\" "+this.resolution;
     }
 }
